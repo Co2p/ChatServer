9951a51e881c33b30d31d356e618b494db9f03bb
co2p <go.co2p@gmail.com>
2014-10-10 15:36:27 +0200

    comments

--- src/eson/co2p/se/clientConnection.java
+++ src/eson/co2p/se/clientConnection.java
@@ -2,6 +2,8 @@ package eson.co2p.se;
 
 /**
  * Created by gordon on 10/10/14.
+ *
+ * A connection to a client, listens to incoming messages and forwards the message to the other klients.
  */
 public class clientConnection {
 }
--- src/eson/co2p/se/nameServerConnection.java
+++ src/eson/co2p/se/nameServerConnection.java
@@ -2,6 +2,8 @@ package eson.co2p.se;
 
 /**
  * Created by gordon on 10/10/14.
+ *
+ * Sends Alive messages to the nameserver.
  */
 public class nameServerConnection {
 }
