1e3b354f6dd202f122a0a88320c78e31335172e3
isidornygren <karlisidornygren@gmail.com>
2014-10-21 18:24:03 +0200

    scoobydooby eyeyeyeyeyeyeyy

---
+++ src/eson/co2p/se/User.java
@@ -0,0 +1,42 @@
+package eson.co2p.se;
+
+import java.net.InetAddress;
+
+/**
+ * Created by root on 2014-10-21.
+ */
+public class User {
+    String nickname;
+    InetAddress ip;
+    int port;
+
+    public User(String nickname, InetAddress ip, int port){
+        setNickname(nickname);
+        setIp(ip);
+        setPort(port);
+    }
+
+    public void setNickname(String nickname){
+        this.nickname = nickname;
+    }
+
+    public void setIp(InetAddress ip){
+        this.ip = ip;
+    }
+
+    public void setPort(int port){
+        this.port = port;
+    }
+
+    public String getNickname(){
+        return nickname;
+    }
+
+    public InetAddress getIp(){
+        return ip;
+    }
+
+    public int getPort(){
+        return port;
+    }
+}
