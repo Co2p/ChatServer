e04bbc7775e5a450c23a0fa485901cbbeda8b238
isidornygren <karlisidornygren@gmail.com>
2014-10-23 00:12:52 +0200

    scoobydooby eyeyeyeyeyeyeyy

--- src/eson/co2p/se/message.java
+++ src/eson/co2p/se/message.java
@@ -70,9 +70,10 @@ public class message {
         try {
             for(User s:users){
                 if(s != null) {
-                    int currentSize = rawdata.length() - 1;
+                    int currentSize = rawdata.length();
                     int nickLength = s.getNickname().getBytes().length;
-                    rawdata.extendTo(currentSize + nickLength);
+                    rawdata.extendTo(currentSize + nickLength + 1);
+                    System.out.println("CurrentSize = " + currentSize + ", nickLength = " + nickLength);
                     rawdata.setSubrange(currentSize, (s.getNickname() + "\0").getBytes("UTF-8"));
                     System.out.println("NICK: " + s.getNickname());
                 }
