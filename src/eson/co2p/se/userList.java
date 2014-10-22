502756fd4b9dbf246456314ae57a4de31e89ef52
co2p <go.co2p@gmail.com>
2014-10-22 16:44:38 +0200

    userList

---
+++ src/eson/co2p/se/userList.java
@@ -0,0 +1,41 @@
+package eson.co2p.se;
+
+import java.util.ArrayList;
+import java.util.Queue;
+
+/**
+ * Created by gordon on 22/10/14.
+ */
+public class userList {
+    private static User[] users = new User[255];
+    private static ArrayList<Integer> removed;
+    private static boolean first;
+
+    public static void addUser(User user){
+        if (first){
+            firstRun();
+        }
+
+        if (!removed.isEmpty()){
+            users[removed.remove(0)] = user;
+        }
+    }
+
+    public static void removeUser(int i){
+        users[i] = null;
+        removed.add(0);
+    }
+
+    public static User getUser(int i){
+        return users[i];
+    }
+
+    private static void firstRun(){
+        int i = 254;
+        for (User user : users) {
+            removed.add(i);
+            i--;
+        }
+    }
+
+}
